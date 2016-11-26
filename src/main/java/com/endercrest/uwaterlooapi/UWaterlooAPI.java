969709e6dd217a7eddc197594827a40a0ee54d2b
endercrest <tcvs@endercrest.com>
2016-11-25 21:41:45 -0500

    Update POM
    POM now follows central repository guidelines. Also includes Feds into UWaterlooAPI.

--- src/main/java/com/endercrest/uwaterlooapi/UWaterlooAPI.java
+++ src/main/java/com/endercrest/uwaterlooapi/UWaterlooAPI.java
@@ -1,6 +1,7 @@
 package com.endercrest.uwaterlooapi;
 
 import com.endercrest.uwaterlooapi.courses.Courses;
+import com.endercrest.uwaterlooapi.feds.Feds;
 
 /**
  * Created by Thomas Cordua-von Specht on 11/23/2016.
@@ -10,16 +11,26 @@ import com.endercrest.uwaterlooapi.courses.Courses;
 public class UWaterlooAPI {
 
     private final Courses courses;
+    private final Feds feds;
 
     public UWaterlooAPI(String api){
         courses = new Courses(api);
+        feds = new Feds(api);
     }
 
     /**
-     * Get the class that contains all API methods. related to courses.
+     * Get the class that contains all API methods for classes.
      * @return {@link Courses} containing api methods.
      */
     public Courses getCourses() {
         return courses;
+    }
+
+    /**
+     * Get the class that contains all API methods for the FEDS.
+     * @return {@link Feds} containing api methods.
+     */
+    public Feds getFeds(){
+        return feds;
     }
 }
